70114a7a14ca75cc0d20a40dd2f7ab16daa945fa
Sean Neary <sean.p.neary@gmail.com>
2026-01-02 16:07:21 -0500

    Add 'Press Space to Honk' instruction to UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -511,7 +511,7 @@ function App() {
       {!isMobile && <h1 style={{ color: '#eee', fontFamily: 'sans-serif', marginBottom: '1rem' }}>FFXIV MSPaint Sim</h1>}
       <div style={{ color: '#aaa', marginBottom: '1rem', fontSize: isMobile ? '0.8rem' : '1rem' }}>
         {myId ? `Connected as ${gameState.players[myId]?.name || myId}` : 'Connecting...'}
-        {!isMobile && <><br />Use W/A/S/D to move. Click and drag in arena to paint.</>}
+        {!isMobile && <><br />Use W/A/S/D to move. Press Space to Honk. Click and drag in arena to paint.</>}
       </div>
       <Arena
         players={gameState.players}
